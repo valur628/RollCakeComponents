2b7ea005f0ea4ea78dd35f07869f170af4c7abf9
Valurauta <wncjf2000@naver.com>
2021-08-09 12:33:11 +0900

    123 123123

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { LargeCardItem } from "./Compo_cards";
 import { MediumCardItem } from "./Compo_cards";
+import { SmallCardItem } from "./Compo_cards";
 
 function App() {
   const abc = "140%";
@@ -24,6 +25,14 @@ function App() {
         DisRate={abc}
         Platform="https://github.com/wncjf2000/RollCakeComponents/blob/main/pic/Steam_icon_logo.png?raw=true"
       ></MediumCardItem>
+      <br></br>
+      <SmallCardItem
+        DevName="마이크로소프트"
+        SoftName="마인크래프트"
+        BeforeCost="30,000"
+        AfterCost="15,000"
+        Platform="https://github.com/wncjf2000/RollCakeComponents/blob/main/pic/Steam_icon_logo.png?raw=true"
+      ></SmallCardItem>
     </>
   );
 }
